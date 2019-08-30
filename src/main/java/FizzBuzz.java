4b3498d65587de5cf7056d7734e543f5e5f2a0fd
Anddd7 <liaoad_space@sina.com>
2019-08-30 16:25:49 +0800

    refactor

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,13 +3,17 @@ public class FizzBuzz {
   public String answer(int input) {
     String result = "";
 
-    if (input % 3 == 0 || String.valueOf(input).contains("3")) {
+    if (isTimesOfOrContains(input, 3)) {
       result += "fizz";
     }
-    if (input % 5 == 0 || String.valueOf(input).contains("5")) {
+    if (isTimesOfOrContains(input, 5)) {
       result += "buzz";
     }
 
     return result.isEmpty() ? String.valueOf(input) : result;
+  }
+
+  private boolean isTimesOfOrContains(int input, int target) {
+    return input % target == 0 || String.valueOf(input).contains(String.valueOf(target));
   }
 }
