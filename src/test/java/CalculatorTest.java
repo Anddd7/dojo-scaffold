9c447ef20a81d349b5a3a83ef55bdf1017186ab2
Anddd7 <liaoad_space@sina.com>
2019-08-30 15:54:18 +0800

    upgrade junit 5 & use assertj

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,11 +1,11 @@
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
-    @Test
-    public void should_be_able_to_run_a_trivial_test_case() {
-        assertThat(Calculator.add(1, 1), is(2));
-    }
+
+  @Test
+  public void should_be_able_to_run_a_trivial_test_case() {
+    assertThat(Calculator.add(1, 1)).isEqualTo(2);
+  }
 }
